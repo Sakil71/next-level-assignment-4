76dc83e21494633213c626d8e898cc1cc7b97f90
Sakil71 <shakilahamed2171@gmail.com>
2025-07-05 23:26:19 +0600

    update

--- src/pages/BorrowSummary/BorrowSummary.tsx
+++ src/pages/BorrowSummary/BorrowSummary.tsx
@@ -11,28 +11,30 @@ export const BorrowSummary = () => {
       <h2 className="text-2xl font-semibold mb-4 text-center">
         Borrow Summary
       </h2>
-      <table className="min-w-full border border-gray-300 divide-y divide-gray-200 text-sm">
-        <thead className="bg-gray-100">
-          <tr>
-            <th className="px-4 py-2 text-left">#</th>
-            <th className="px-4 py-2 text-left">Book Title</th>
-            <th className="px-4 py-2 text-left">ISBN</th>
-            <th className="px-4 py-2 text-left">Total Quantity Borrowed</th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-200">
-          {data?.summary.map((item, index) => (
-            <tr key={index}>
-              <td className="px-4 py-2 font-semibold">
-                {index + 1}.
-              </td>
-              <td className="px-4 py-2 font-semibold">{item.book.title}</td>
-              <td className="px-4 py-2">{item.book.isbn}</td>
-              <td className="px-4 py-2 font-semibold">{item.totalQuantity}</td>
+      <div className="overflow-x-auto">
+        <table className="min-w-full border border-gray-300 divide-y divide-gray-200 text-sm">
+          <thead className="bg-gray-100">
+            <tr>
+              <th className="px-4 py-2 text-left">#</th>
+              <th className="px-4 py-2 text-left">Book Title</th>
+              <th className="px-4 py-2 text-left">ISBN</th>
+              <th className="px-4 py-2 text-left">Total Quantity Borrowed</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="divide-y divide-gray-200">
+            {data?.summary.map((item, index) => (
+              <tr key={index}>
+                <td className="px-4 py-2 font-semibold">{index + 1}.</td>
+                <td className="px-4 py-2 font-semibold">{item.book.title}</td>
+                <td className="px-4 py-2">{item.book.isbn}</td>
+                <td className="px-4 py-2 font-semibold">
+                  {item.totalQuantity}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
